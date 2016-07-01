0994b3333a67b96314b04aaa8737e35539b04978
kdo <otwmoo@gmail.com>
2016-07-01 14:55:31 +0900

    인라인 스타일링 예제

--- chapter02/src/App.js
+++ chapter02/src/App.js
@@ -1,6 +1,12 @@
+/**
+ * 커스텀 팩토리 예제
+ */
 import React, {Component} from 'react';
 import {render} from 'react-dom';
 
+/**
+ * 그리팅
+ */
 class Greeting extends Component {
     render() {
         return (<span>hi</span>);
@@ -16,4 +22,27 @@ render(
         greeting({name: "World"})
     ),
     document.getElementById('root')
+);
+
+/**
+ * 인라인 스타일링 예제
+ */
+
+const style = {
+    container: {
+        backgroundColor: "#ddd",
+        width: 900
+    }
+}
+
+class Container extends Component {
+    render() {
+        return (
+            <div style={style.container}>test</div>
+        );
+    }
+}
+
+render(
+    <Container/>, document.getElementById('second')
 );
