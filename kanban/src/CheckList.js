f8cd4cf9842995aec3de32779fc68e2472acc614
otwm <otwmoo@gmail.com>
2016-07-16 14:28:27 +0900

    task add

--- kanban/src/CheckList.js
+++ kanban/src/CheckList.js
@@ -13,6 +13,9 @@ class CheckList extends Component {
         return (
             <div className="checklist">
                 <ul>{tasks}</ul>
+                <input type="text"
+                    className="checklist--add-task"
+                    placeholder="Type then hit Enter to add a task"/>
             </div>
         );
     }
