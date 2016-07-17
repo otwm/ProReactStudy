f60e6716bfcb195a5fac75093689ac197ab56483
otwm <otwmoo@gmail.com>
2016-07-17 11:17:58 +0900

    칸반 앱: 속성 형식 정의 오타 수정

--- kanban/src/Card.js
+++ kanban/src/Card.js
@@ -2,6 +2,17 @@ import React, {Component, PropTypes} from 'react';
 import CheckList from './CheckList'
 import marked from 'marked';
 
+let titlePropType = (props, propName, componentName) => {
+    if (props[propName]) {
+        let value = props[propName];
+        if (typeof value !== 'string' || value.length > 80) {
+            return new Error(
+                `${propName} in ${componentName} is longer than 80 characters`
+            );
+        }
+    }
+};
+
 /**
  * 카드 컴퍼넌트
  */
@@ -61,7 +72,7 @@ class Card extends Component {
 
 Card.propTypes = {
     id: PropTypes.number,
-    title: PropTypes.string,
+    title: titlePropType,
     description: PropTypes.string,
     color: PropTypes.string,
     tasks: PropTypes.arrayOf(PropTypes.object)
