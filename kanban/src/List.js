bf74d50ec9029917a178a6321bc392600d82ab44
otwm <otwmoo@gmail.com>
2016-07-21 16:45:55 +0900

    taskCallbacks 추가 (리스트)

--- kanban/src/List.js
+++ kanban/src/List.js
@@ -4,9 +4,10 @@ import Card from './Card'
 class List extends Component {
 
     render() {
-        var cards = this.props.cards.map((card) => {
+        let cards = this.props.cards.map((card) => {
             return <Card
                 key={card.id}
+                taskCallbacks={this.props.taskCallbacks}
                 id={card.id}
                 title={card.title}
                 description={card.description}
@@ -27,6 +28,7 @@ class List extends Component {
 List.propTypes = {
     title: PropTypes.string.isRequired
     , cards: PropTypes.arrayOf(PropTypes.object)
+    , taskCallbacks: PropTypes.object
 }
 
 export default List;
