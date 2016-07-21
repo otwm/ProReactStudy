48693cc50af1a9f26b9e13a3fe40f14132fd25a6
otwm <otwmoo@gmail.com>
2016-07-21 16:48:19 +0900

    taskCallbacks 추가 (카드)

--- kanban/src/Card.js
+++ kanban/src/Card.js
@@ -37,7 +37,11 @@ class Card extends Component {
             cardDetails = (
                 <div className="card__details">
                     <span dangerouslySetInnerHTML={{__html: marked(this.props.description)}}/>
-                    <CheckList cardId={this.props.id} tasks={this.props.tasks}/>
+                    <CheckList
+                        cardId={this.props.id}
+                        tasks={this.props.tasks}
+                        taskCallbacks={this.props.taskCallbacks}
+                    />
                 </div>
             );
         }
@@ -75,7 +79,8 @@ Card.propTypes = {
     title: titlePropType,
     description: PropTypes.string,
     color: PropTypes.string,
-    tasks: PropTypes.arrayOf(PropTypes.object)
+    tasks: PropTypes.arrayOf(PropTypes.object),
+    taskCallbacks: PropTypes.object
 };
 
 export default Card;
