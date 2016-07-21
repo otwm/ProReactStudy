2fa0660941da0692b03f2ae4db872c4299f95915
otwm <otwmoo@gmail.com>
2016-07-21 16:53:48 +0900

    taskCallbacks 추가 (체크 리스트)

--- kanban/src/CheckList.js
+++ kanban/src/CheckList.js
@@ -2,11 +2,20 @@ import React, {Component, PropTypes} from 'react';
 
 class CheckList extends Component {
     render() {
-        let tasks = this.props.tasks.map((task) => (
-            <li key={task.id}>
-                <input type="checkbox" defaultChecked={task.done}/>
+        let tasks = this.props.tasks.map((task, taskIndex) => (
+            <li key={task.id} className="checklist__task">
+                <input type="checkbox"
+                       checked={task.done}
+                       onChange={
+                           this.props.taskCallbacks.toggle.bind(null, this.props.cardId, task.id, taskIndex)
+                       }
+                />
                 {task.name}{' '}
-                <a href="#" className="checklist__task--remove"/>
+                <a href="#" className="checklist__task--remove"
+                   onClick={
+                       this.props.taskCallbacks.delete.bind(null, this.props.cardId, task.id, taskIndex)
+                   }
+                />
             </li>
         ));
 
