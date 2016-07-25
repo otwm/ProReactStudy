b088e16c472b0f3f316c8cb09259a0751b8fbed0
otwm <otwmoo@gmail.com>
2016-07-25 15:25:26 +0900

    [4장] 리액트 DnD 9(Snack)

--- chapter04/src/Snack.js
+++ chapter04/src/Snack.js
@@ -1,6 +1,29 @@
 import React, {PropTypes, Component} from 'react';
 import {DragSource} from 'react-dnd';
 
+const snackSpec = {
+    beginDrag(props){
+        return {
+            name: props.name
+        };
+    },
+    endDrag(props, monitor){
+        const dragItem = monitor.getItem();
+        const dropResult = monitor.getDropResult();
+
+        if (dropResult) {
+            console.log(`You dropped ${dragItem.name} into ${dropResult.name}`);
+        }
+    }
+};
+
+let collect = (connect, monitor)=> {
+    return {
+        connectDragSource: connect.dragSource(),
+        isDragging: monitor.isDragging()
+    }
+};
+
 class Snack extends Component {
     render() {
         const {name} = this.props;
@@ -20,3 +43,5 @@ class Snack extends Component {
 Snack.propTypes = {
     name: PropTypes.string.isRequired
 };
+
+export default DragSource('snack', snackSpec, collect)(Snack);
