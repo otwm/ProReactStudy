01f84f6a42a6a13ecc89286f208ac4c916408474
otwm <otwmoo@gmail.com>
2016-07-30 12:33:55 +0900

    사진 추가

--- kanban/src/utils.js
+++ kanban/src/utils.js
@@ -1,5 +1,11 @@
 import 'babel-polyfill';
 
+/**
+ *
+ * @param func
+ * @param wait
+ * @returns {Function}
+ */
 export const throttle = (func, wait) => {
     let context, args, prevArgs, argsChanged, result;
     let previous = 0;
@@ -13,6 +19,10 @@ export const throttle = (func, wait) => {
         args = arguments;
         argsChanged = JSON.stringify(args) != JSON.stringify(prevArgs);
         prevArgs = Object.assign({}, args);
+        /**
+         * 1. 인자가 변경 되었거나
+         * 2. wait 가 설정 되어 있는 경우 wait 만큼 다시 시간이 훌렀을 때 true
+         */
         if (argsChanged || wait && (remaining <= 0 || remaining > wait)) {
             if (wait) {
                 previous = now;
