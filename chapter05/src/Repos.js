7dfcf833b776ff7dd20b016f355cbe0af34e8cf8
otwm <otwmoo@gmail.com>
2016-08-06 06:38:09 +0900

    [5장]리엑트 라우트 step3

--- chapter05/src/Repos.js
+++ chapter05/src/Repos.js
@@ -1,9 +1,34 @@
 import React, {Component} from 'react';
+import 'whatwg-fetch';
 
 class Repos extends Component {
+
+    constructor() {
+        super(...arguments);
+        this.state = {
+            repositories: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://api.github.com/users/pro-react/repos')
+            .then((response) => response.json())
+            .then((responseData) => {
+                this.setState({repositories: responseData})
+            });
+    }
+
     render() {
+        let repos = this.state.repositories.map((repo) => (
+            <li key={repo.id}>{repo.name}</li>
+        ));
         return (
-            <h1>Repos</h1>
+            <div>
+                <h1>Github Repos</h1>
+                <ul>
+                    {repos}
+                </ul>
+            </div>
         );
     }
 }
