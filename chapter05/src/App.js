4df4ca1ad1ca149308eb4bcaf843e5e44905a64c
otwm <otwmoo@gmail.com>
2016-08-05 12:31:02 +0900

    [5장] step2

--- chapter05/src/App.js
+++ chapter05/src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import {render} from 'react-dom';
 
 import About from './About';
-import Home from './home';
+import Home from './Home';
 import Repos from './Repos';
 
 class App extends Component {
@@ -15,10 +15,38 @@ class App extends Component {
 
     componentDidMount() {
         window.addEventListener('hashchange', ()=> {
+            console.log('test');
             this.setState({
                 route: window.location.hash.substr(1)
             })
         });
+    }
+
+    render() {
+        var Child;
+        switch (this.state.route) {
+            case '/about':
+                Child = About;
+                break;
+            case '/repos':
+                Child = Repos;
+                break;
+            default:
+                Child = Home;
+        }
+
+        return (
+            <div>
+                <header>App</header>
+                <menu>
+                    <ul>
+                        <li><a href="#/about">About</a></li>
+                        <li><a href="#/repos">Repos</a></li>
+                    </ul>
+                </menu>
+                <Child/>
+            </div>
+        );
     }
 }
 
---
+++ chapter05/src/Repos.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Repos extends Component {
+    render() {
+        return (
+            <h1>Repos</h1>
+        );
+    }
+}
+
+export default Repos;
