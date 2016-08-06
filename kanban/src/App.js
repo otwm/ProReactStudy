5149b54ccae6bf1888dbe9010d6ed342b5694792
otwm <otwmoo@gmail.com>
2016-08-06 23:10:38 +0900

    [5장]kanban step4( 라우트 설정 )

--- kanban/src/App.js
+++ kanban/src/App.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {Router, Route} from 'react-router';
+import createBrowserHistory from 'history/lib/createBrowserHistory'
 import KanbanBoardContainer from './KanbanBoardContainer';
+import KanbanBoard from './KanbanBoard';
+import EditCard from './EditCard';
+import NewCard from './NewCard';
 
-ReactDOM.render(<KanbanBoardContainer/>, document.getElementById('root'));
+ReactDOM.render((
+    <Router history={createBrowserHistory()}>
+        <Route component={KanbanBoardContainer}>
+            <Route path="/" component={KanbanBoard}>
+                <Route path="new" component={NewCard}/>
+                <Route path="edit/:card_id" component={EditCard}/>
+            </Route>
+        </Route>
+    </Router>
+), document.getElementById('root'));
