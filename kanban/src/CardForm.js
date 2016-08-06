db94554efadd86e0bdc68b8aff36d2ccc2f7b702
otwm <otwmoo@gmail.com>
2016-08-07 00:25:33 +0900

    [5장]kanban step7( +추가 )

--- kanban/src/CardForm.js
+++ kanban/src/CardForm.js
@@ -1,51 +1,54 @@
 import React, {Component, PropTypes} from 'react';
 
 class CardForm extends Component {
-    handleChange(field, e) {
+
+    handleChange(field, e){
         this.props.handleChange(field, e.target.value);
     }
 
-    handleClose(e) {
+    handleClose(e){
         e.preventDefault();
         this.props.handleClose();
     }
 
-    render() {
+    render(){
         return (
             <div>
                 <div className="card big">
                     <form onSubmit={this.props.handleSubmit.bind(this)}>
-                        <input type="text"
+                        <input type='text'
                                value={this.props.draftCard.title}
-                               onChange={this.handleChange.bind(this, 'title')}
+                               onChange={this.handleChange.bind(this,'title')}
                                placeholder="Title"
                                required={true}
-                               autoFocus={true}/>
+                               autoFocus={true} /><br />
                         <textarea value={this.props.draftCard.description}
-                                  onChange={this.handleChange.bind(this, 'description')}
+                                  onChange={this.handleChange.bind(this,'description')}
                                   placeholder="Description"
-                                  required={true}/>
+                                  required={true} /><br />
                         <label htmlFor="status">Status</label>
                         <select id="status"
                                 value={this.props.draftCard.status}
-                                onChange={this.handleChange.bind(this, 'status')}>
+                                onChange={this.handleChange.bind(this,'status')}>
                             <option value="todo">To Do</option>
                             <option value="in-progress">In Progress</option>
                             <option value="done">Done</option>
                         </select>
-                        <br/>
+                        <br />
                         <label htmlFor="color">Color</label>
                         <input id="color"
                                value={this.props.draftCard.color}
-                               onChange={this.handleChange.bind(this, 'color')}
+                               onChange={this.handleChange.bind(this,'color')}
                                type="color"
-                               defaultValue="#ff0000"/>
-                        <div className="actions">
+                               defaultValue="#ff0000" />
+
+                        <div className='actions'>
                             <button type="submit">{this.props.buttonLabel}</button>
                         </div>
                     </form>
                 </div>
-                <div className="overlay" onClick={this.handleClose.bind(this)}></div>
+                <div className="overlay" onClick={this.handleClose.bind(this)}>
+                </div>
             </div>
         );
     }
@@ -62,6 +65,6 @@ CardForm.propTypes = {
     handleChange: PropTypes.func.isRequired,
     handleSubmit: PropTypes.func.isRequired,
     handleClose: PropTypes.func.isRequired,
-};
+}
 
 export default CardForm;
