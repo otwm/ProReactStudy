9ec6649035073012f294307cc36ebf2729bf7332
otwm <otwmoo@gmail.com>
2016-08-27 17:12:46 +0900

    Store util 사용

--- chapter06/myBank/app/BankBalanceStore.js
+++ chapter06/myBank/app/BankBalanceStore.js
@@ -1,17 +1,6 @@
-import {EventEmitter} from 'fbemitter';
 import AppDispatcher from './AppDispatcher';
+import {Store} from 'flux/utils'
 import bankConstants from './constants';
-
-/**
- * 변경 이벤트
- * @type {string}
- */
-const CHANGE_EVENT = 'change';
-
-/**
- * 이벤트 에미터
- */
-let __emitter = new EventEmitter();
 
 /**
  * 계좌 금액
@@ -19,46 +8,41 @@ let __emitter = new EventEmitter();
  */
 let balance = 0;
 
-let BankBalanceStore = {
+/**
+ * 스토어
+ */
+class BankBalanceStore extends Store {
     /**
-     * 저장된 상태를 반환 한다.
+     * 상태 값
      * @returns {number}
      */
-    getState(){
+    getState() {
         return balance;
-    },
+    }
+
     /**
-     * 리스너를 등록 한다.
-     * 컴퍼넌트 상에서 사용할 콜백을 등록하기 위해 사용한다.
-     * @param callback 콜백
-     * @returns {*} 리스너 제거용 토큰
+     * 디스패치
+     * @param action
+     * @private
      */
-    addListener: (callback) => {
-        console.log('addListener');
-        return __emitter.addListener(CHANGE_EVENT, callback);
+    __onDispatch(action) {
+        console.log('__onDispatch');
+        switch (action.type) {
+            case bankConstants.CREATED_ACCOUNT:
+                balance = 0;
+                //사실 현 소스 상 이 부분의 이밋은 의미가 없다.
+                this.__emitChange();
+                break;
+            case  bankConstants.DEPOSITED_INTO_ACCOUNT:
+                balance = balance + action.amount;
+                this.__emitChange();
+                break;
+            case bankConstants.WITHDREW_FROM_ACCOUNT:
+                balance = balance - action.amount;
+                this.__emitChange();
+                break;
+        }
     }
-};
+}
 
-/**
- * 디스패쳐에 액션을 등록 했다.
- * 액션이 수행되면 기본적으로 emitter를 통해 컴퍼넌트에 값을 알려준다.
- */
-BankBalanceStore.dispatchToken = AppDispatcher.register((action) => {
-    switch (action.type) {
-        case bankConstants.CREATED_ACCOUNT:
-            balance = 0;
-            //사실 현 소스 상 이 부분의 이밋은 의미가 없다.
-            __emitter.emit(CHANGE_EVENT);
-            break;
-        case  bankConstants.DEPOSITED_INTO_ACCOUNT:
-            balance = balance + action.amount;
-            __emitter.emit(CHANGE_EVENT);
-            break;
-        case bankConstants.WITHDREW_FROM_ACCOUNT:
-            balance = balance - action.amount;
-            __emitter.emit(CHANGE_EVENT);
-            break;
-    }
-});
-
-export default BankBalanceStore;
+export default new BankBalanceStore(AppDispatcher);
