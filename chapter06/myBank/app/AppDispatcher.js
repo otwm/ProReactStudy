f9a95399a822e685825d64abb25772f8290a350b
otwm <otwmoo@gmail.com>
2016-08-27 14:27:10 +0900

    디스 패처 로깅

--- chapter06/myBank/app/AppDispatcher.js
+++ chapter06/myBank/app/AppDispatcher.js
@@ -1,2 +1,10 @@
 import {Dispatcher} from 'flux';
-export default new Dispatcher();
+
+class AppDispatcher extends Dispatcher {
+    dispatch(action = {}) {
+        console.log('Dispatched', action);
+        super.dispatch(action);
+    }
+}
+
+export default new AppDispatcher();
