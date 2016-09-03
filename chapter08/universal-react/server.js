66b6f9033dc11ffe00a5c3b7367db1d9d39dff5f
otwm <otwmoo@gmail.com>
2016-09-03 16:36:26 +0900

    서버 렌더링

--- chapter08/universal-react/server.js
+++ chapter08/universal-react/server.js
@@ -1,15 +1,23 @@
-import express from 'express';
-import contacts from './public/contacts.json';
+import fs from "fs";
+import express from "express";
+import React from "react";
+import {renderToString} from "react-dom/server";
+import ContactsApp from "./app/components/ContactsApp";
 
 const app = express();
 
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 app.set('views', './');
 app.use(express.static(__dirname + './public'));
 
+const contacts = JSON.parse(fs.readFileSync(__dirname + '/public/contacts.json', 'utf8'));
+
+const ContactsAppFactory = React.createFactory(ContactsApp);
+
 app.get('/', (request, response) => {
+    let componentInstance = ContactsAppFactory({initialData: contacts});
     response.render('index', {
-        content: 'Hello'
+        content: renderToString(componentInstance)
     });
 });
 
